6868091fcee9b2be301ec18211c7bab7323e81e7
Gabriel Bevilacqua Barros <92558878+GabrielBBarros@users.noreply.github.com>
2023-04-26 15:11:57 -0300

    Update Aula-13-14.asm

--- Aula-13-14.asm
+++ Aula-13-14.asm
@@ -1,1 +1,22 @@
+1A)
 
+CLD
+MOV AL, A4
+MOV CL, 20
+MOV DI, 200
+REPNE SCASB -> Compara Strings de Byte na memória 
+DEC DI
+JZ xxx 
+MOV BX, DI
+MOV AL, 00
+INT 20
+
+-> MOV BX, DI
+   MOV AL, 01
+   INT 20
+
+________________________________________________________________________________________________________________
+
+1B)
+
+MOV CL, 20
