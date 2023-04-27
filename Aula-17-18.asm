f65ee747b0dd55f79f3d97397cd1e1ae91454750
Gabriel Bevilacqua Barros <92558878+GabrielBBarros@users.noreply.github.com>
2023-04-27 16:28:12 -0300

    Update Aula-17-18.asm

--- Aula-17-18.asm
+++ Aula-17-18.asm
@@ -1,1 +1,22 @@
 
+1)
+//Jeito médio
+mov dh, 4
+mov cx, 101
+mov si, 200
+->mov al, [si]
+shl al, 2
+mov [si], al 
+inc si
+<-loop
+int 20
+
+
+
+//Jeito fácil
+1)
+mov cx, 101
+mov si, 200
+->shl byte ptr [si], 2
+inc si
+<-loop
